60b3ffcf5ffee399b4676c00877f19d18a77292f
Diana Paievska <71642076+paievska@users.noreply.github.com>
2023-12-12 21:14:52 +0200

    add test

--- module15/src/test/java/com/example/module15/Module15ApplicationTests.java
+++ module15/src/test/java/com/example/module15/Module15ApplicationTests.java
@@ -1,14 +1,22 @@
 package com.example.module15;
 
+import com.example.module15.entities.Note;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class Module15ApplicationTests {
 
 	@Test
-	void contextLoads() {
-		
+	void testNote() {
+		String expectedTitle = "Test Title";
+		String content = "Test Content";
+
+		Note note = new Note(expectedTitle, content);
+
+		assertEquals(expectedTitle, note.getTitle());
 	}
 
 }
